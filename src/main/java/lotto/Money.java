73c520b6c9dd5379f7a5fe2f030c05ba41b7eea9
023-dev <0_2_3@naver.com>
2025-11-03 19:10:07 +0900

    feat: 금액 범위 유효성 검사 및 예외 처리 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,4 +1,29 @@
 package lotto;
 
+import static lotto.exception.ExceptionMessage.INVALID_MONEY_RANGE;
+
+import lotto.exception.LottoException;
+
 public record Money(int amount) {
+    private static final int MIN_AMOUNT = 1_000;
+    private static final int MAX_AMOUNT = 2_000_000_000;
+
+    public Money {
+        validate(amount);
+    }
+
+    private void validate(final int amount) {
+        validateRange(amount);
+    }
+
+    private void validateRange(final int amount) {
+        if (isOutOfRange(amount)) {
+            throw new LottoException(INVALID_MONEY_RANGE);
+        }
+    }
+
+    private boolean isOutOfRange(final int amount) {
+        return amount < MIN_AMOUNT ||
+                amount > MAX_AMOUNT;
+    }
 }
--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,20 +1,43 @@
 package lotto;
 
+import static lotto.exception.ExceptionMessage.INVALID_MONEY_RANGE;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.base.TestSupport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest extends TestSupport {
 
-    @Test
-    void 정수로_생성할_수_있다() {
-        // given
-        int amount = 5000;
-
+    @ParameterizedTest
+    @ValueSource(ints = {1_000, 5_000, 2_000_000_000})
+    void 정수로_생성할_수_있다(int amount) {
         // expected
         assertThatCode(() -> new Money(amount))
                 .doesNotThrowAnyException();
     }
-}
 
+    @Test
+    void 최소값보다_작은_금액은_예외가_발생한다() {
+        // given
+        int amount = 999;
+
+        // expected
+        assertThatThrownBy(() -> new Money(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_MONEY_RANGE.getMessage());
+    }
+
+    @Test
+    void 최대값보다_큰_금액은_예외가_발생한다() {
+        // given
+        int amount = 2_000_000_001;
+
+        // expected
+        assertThatThrownBy(() -> new Money(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_MONEY_RANGE.getMessage());
+    }
+}
