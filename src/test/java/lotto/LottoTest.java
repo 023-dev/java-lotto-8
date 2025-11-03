c178f2290cca05f25696ce7d57a6ef26b99d1fc6
023-dev <0_2_3@naver.com>
2025-11-03 18:58:36 +0900

    test: 로또 번호 생성 관련 성공 케이스 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static lotto.exception.ExceptionMessage.INVALID_LOTTO_NUMBER_DUPLICATE;
 import static lotto.exception.ExceptionMessage.INVALID_LOTTO_NUMBER_SIZE;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -9,6 +10,27 @@ import lotto.base.TestSupport;
 import org.junit.jupiter.api.Test;
 
 class LottoTest extends TestSupport {
+
+    @Test
+    void 로또는_6개의_로또_번호로_생성할_수_있다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // expected
+        assertThatCode(() -> Lotto.from(numbers))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 최소값과_최대값을_포함한_로또_번호로_생성할_수_있다() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 43, 44, 45);
+
+        // expected
+        assertThatCode(() -> Lotto.from(numbers))
+                .doesNotThrowAnyException();
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         // given
@@ -21,7 +43,8 @@ class LottoTest extends TestSupport {
     }
 
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {// given
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        // given
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 5);
 
         // expected
